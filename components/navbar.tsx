4f6dcf3d426447b39bf477c87bbfb28ac2ddcf0f
AdamAlAttrach <adamalattrach@gmail.com>
2025-02-24 21:07:09 -0500

    removed unused variables

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,11 @@
 "use client"
 
 import * as React from "react"
-import Link from "next/link"
 import { ModeToggle } from "@/components/ui/mode-toggle"
-
-import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
 
