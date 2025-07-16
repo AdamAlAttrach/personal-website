cb808d62bfa4f5176acc39076a56b4f43e57a2fe
AdamAlAttrach <adamalattrach@gmail.com>
2025-07-16 18:45:46 -0400

    changed about me

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -9,7 +9,7 @@ export function AboutMe() {
             <p className="scroll-m-20 mt-4 text-lg text-gray-500 lg:text-2xl max-w-xl about-p">
                 Hullo! I’m Adam Al-Attrach—born and raised in Montréal, Canada. 
                 I am studying Computer Science/Statistics at McGill. I love solving problems and learning along the way. 
-                In my spare time, you’ll find me reading, working out, or working on personal projects.
+                In my spare time, you’ll find me drawing, playing guitar or working out.
             </p>
         </div>
     )
